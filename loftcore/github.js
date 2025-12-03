1e3a8ed0aef5fee80d794fcbe2fae82363af1ea7
xmdloft23 <xmdloft745@gmail.com>
2025-12-03 13:13:21 +0300

    Update github.js

--- loftcore/github.js
+++ loftcore/github.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/xmdloft23/quantum-bot');
+    const res = await fetch('https://api.github.com/repos/xmdloft23/loft-quantum');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
